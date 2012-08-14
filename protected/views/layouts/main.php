1b96b4e1e983e489845ed300e81c98855f4c4fa5
Jiacong Zhong <zhongjiacong@gmail.com>
2012-08-15 02:36:18 +0800

    clock

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,6 +32,37 @@
             Yii::app()->theme->baseUrl.'/js/artDialog/artDialog.min.js', CClientScript::POS_HEAD);
         Yii::app()->clientScript->registerScriptFile(
             Yii::app()->theme->baseUrl.'/js/artDialog/artDialog.plugins.min.js', CClientScript::POS_HEAD);
+		Yii::app()->clientScript->registerScript('clock','
+              setInterval( function() {
+              var seconds = new Date().getSeconds();
+              var sdegree = seconds * 6;
+              var srotate = "rotate(" + sdegree + "deg)";
+              
+              $("#sec").css({"-moz-transform" : srotate, "-webkit-transform" : srotate});
+                  
+              }, 1000 );
+              
+         
+              setInterval( function() {
+              var hours = new Date().getHours();
+              var mins = new Date().getMinutes();
+              var hdegree = hours * 30 + (mins / 2);
+              var hrotate = "rotate(" + hdegree + "deg)";
+              
+              $("#hour").css({"-moz-transform" : hrotate, "-webkit-transform" : hrotate});
+                  
+              }, 1000 );
+        
+        
+              setInterval( function() {
+              var mins = new Date().getMinutes();
+              var mdegree = mins * 6;
+              var mrotate = "rotate(" + mdegree + "deg)";
+              
+              $("#min").css({"-moz-transform" : mrotate, "-webkit-transform" : mrotate});
+                  
+              }, 1000 );
+		');
 	?>
 
 </head>
@@ -42,6 +73,11 @@
 	<div id="headerline">&nbsp;</div>
 	<div id="header">
 		<div id="headright">
+			<ul id="clock">	
+			   	<li id="sec"></li>
+			   	<li id="hour"></li>
+				<li id="min"></li>
+			</ul>
 			<div id="syslangdiv">
 				<?php
 					$syslang = array(
