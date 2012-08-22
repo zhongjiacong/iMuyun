a4bdfcbae66c81ce8250c04da59eb0d15beec30c
Jiacong Zhong <zhongjiacong@gmail.com>
2012-08-22 15:47:33 +0800

    test

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+/** 
+* Read Docx File 
+* 
+* @param string $file filepath 
+* @return string file content 
+*/ 
+function parseWord($file) { 
+    $content = ""; 
+    $zip = new ZipArchive ( ); 
+    if ($zip->open ( $file ) === tr ) { 
+        for($i = 0; $i < $zip->numFiles; $i ++) { 
+            $entry = $zip->getNameIndex ( $i ); 
+            if (pathinfo ( $entry, PATHINFO_BASENAME ) == "document.xml") { 
+                $zip->extractTo ( pathinfo ( $file, PATHINFO_DIRNAME ) . "/" . pathinfo ( $file, PATHINFO_FILENAME ), array ( 
+                        $entry 
+                ) ); 
+                $filepath = pathinfo ( $file, PATHINFO_DIRNAME ) . "/" . pathinfo ( $file, PATHINFO_FILENAME ) . "/" . $entry; 
+                $content = strip_tags ( file_get_contents ( $filepath ) ); 
+                break; 
+            } 
+        } 
+        $zip->close (); 
+        rrmdir ( pathinfo ( $file, PATHINFO_DIRNAME ) . "/" . pathinfo ( $file, PATHINFO_FILENAME ) ); 
+        return $content; 
+    } else { 
+        return ""; 
+    } 
+}
+
+echo parseWord(dirname(__FILE__).'/public/file/test.docx');
+?>
