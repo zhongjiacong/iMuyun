7b48895a4d0a4b9fec1f1307e0d18f3af02a8477
Jiacong Zhong <zhongjiacong@gmail.com>
2012-09-17 23:20:05 +0800

    consume

---
+++ protected/extensions/alipay/return.php
@@ -0,0 +1,31 @@
+<?php
+require_once dirname(__FILE__)."/alipay.config.php";
+require_once dirname(__FILE__)."/lib/alipay_notify.class.php";
+
+//计算得出通知验证结果
+$alipayNotify = new AlipayNotify($aliapy_config);
+$verify_result = $alipayNotify->verifyReturn();
+if($verify_result) {
+    //获取支付宝的通知返回参数，可参考技术文档中页面跳转同步通知参数列表
+    $out_trade_no	= $_GET['out_trade_no'];	//获取订单号
+    $trade_no		= $_GET['trade_no'];		//获取支付宝交易号
+    $total_fee		= $_GET['total_fee'];		//获取总价格
+
+    if($_GET['trade_status'] == 'TRADE_FINISHED' || $_GET['trade_status'] == 'TRADE_SUCCESS') {
+		//判断该笔订单是否在商户网站中已经做过处理
+			//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+			//如果有做过处理，不执行商户的业务程序
+		logResult($out_trade_no." auditing\n");
+		Consume::model()->updateByPk($out_trade_no,array("audit"=>1));
+		logResult($out_trade_no." audited\n");
+		$this->redirect(array('consume/create'));
+    }
+    else
+		echo "trade_status=".$_GET['trade_status'];
+}
+else {
+    //验证失败
+    //如要调试，请看alipay_notify.php页面的verifyReturn函数，比对sign和mysign的值是否相等，或者检查$responseTxt有没有返回true
+    echo "验证失败";
+}
+?>
