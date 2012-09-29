3f8387a47de00c233690079ef3129e6f39d1f851
Jiacong Zhong <zhongjiacong@gmail.com>
2012-09-29 23:11:26 +0800

    price

--- protected/models/Article.php
+++ protected/models/Article.php
@@ -164,6 +164,7 @@ class Article extends CActiveRecord
 			case 1:
 				$wordcount = str_word_count(
 					preg_replace("/[\x{4e00}-\x{9fff}\x{f900}-\x{faff}]/u", " ", $content));
+				$price = Article::model()->difficultyCoefficient($srclang_id, $content) * $wordcount * 0.12;
 				break;
 			/*case 2:
 				$wordcount = strlen(preg_replace("/ +/", " ", $content)) - strlen(preg_replace("/ +/", "", $content));
--- protected/views/article/test.php
+++ protected/views/article/test.php
@@ -1,3 +1,11 @@
 <?php
-	echo Article::model()->difficultyCoefficient(1,"duplicating");
+	$so = scws_new();
+	$so->set_charset('utf8');
+	// 这里没有调用 set_dict 和 set_rule 系统会自动试调用 ini 中指定路径下的词典和规则文件
+	$so->send_text("我是一个中国人,我会C++语言,我也有很多T恤衣服");
+	while ($tmp = $so->get_result())
+	{
+	  print_r($tmp);
+	}
+	$so->close();
 ?>
